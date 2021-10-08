70cefd9e93b2a1e601f038752cc97203367f9b15
Oli4B <61087033+Oli4B@users.noreply.github.com>
2021-10-08 12:11:45 +0200

    Update 28.1 numerical integration.py

--- 28.1 numerical integration.py
+++ 28.1 numerical integration.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.integrate as integrate
 
 def func(m):
+    """Function that we integrated numerically"""
     return (1 + 0.3 * m) * (1 + 0.5 * m) * (1 + 0.7 * m) * (1 + 0.8 * m) * (1 + 0.9 * m)
 
 print(integrate.quad(func, -1, 1)[0]/64)
