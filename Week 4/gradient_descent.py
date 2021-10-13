a98ea409c4a84bb9ba3a44b4c40d330c9123a54e
Olivier Brahma <olivier.brahma@gmail.com>
2021-10-13 16:17:53 +0200

    Geen idee wat er mis gaat

--- Week 4/gradient_descent.py
+++ Week 4/gradient_descent.py
@@ -1,64 +1,168 @@
-# http://yann.lecun.com/exdb/mnist/
-# ! pip3 install mlxtend
 import numpy as np
+import matplotlib.pyplot as plt
 from mlxtend.data import loadlocal_mnist
+from tqdm import tqdm
 
+def load_data(images_path, labels_path):
+    # Load data
+    x, y = loadlocal_mnist(
+        images_path=images_path, 
+        labels_path=labels_path
+    )
 
-# Load data
-train_X, train_y = loadlocal_mnist(
-    images_path='data/train-images-idx3-ubyte', 
-    labels_path='data/train-labels-idx1-ubyte'
-)
+    # Take the 3's and 7's
+    x_3 = np.array([val for idx, val in enumerate(x) if y[idx] == 3])
+    x_7 = np.array([val for idx, val in enumerate(x) if y[idx] == 7])
 
-test_X, test_y = loadlocal_mnist(
-    images_path='data/t10k-images-idx3-ubyte', 
-    labels_path='data/t10k-labels-idx1-ubyte'
-)
+    # 0: x = 3, 1: x = 7
+    x = np.concatenate((x_3, x_7))
+    t = np.concatenate((np.zeros(len(x_3)), np.ones(len(x_7))))
 
-#print('Dimensions train data: %s x %s' % (train_X.shape[0], train_X.shape[1]))
+    x = x / 255                    # normalise x
+    x = np.insert(x, 0, 1, axis=1) # append 1 for w_0
 
-# --------------------------------------
-# Gradient methods
-train_X_3 = np.array([val for idx, val in enumerate(train_X) if train_y[idx] == 3])
-train_X_7 = np.array([val for idx, val in enumerate(train_X) if train_y[idx] == 7])
+    return x, t
 
-# print('Dimensions X_3: %s x %s' % (train_X_3.shape[0], train_X_3.shape[1]))
-# print('Dimensions X_7: %s x %s' % (train_X_7.shape[0], train_X_7.shape[1]))
-
-# --------------------------------------
-# Gradient descent
-
-# Hyperparamaters
-learning_rate = 0.001 # n
-
-# 0: x = 3, 1: x = 7
-train_X = np.concatenate((train_X_3, train_X_7))
-train_y = np.concatenate((np.zeros(train_X_3.shape[0]), np.ones(train_X_7.shape[0])))
-
-print('Dimensions train_X: %s x %s' % (train_X.shape[0], train_X.shape[1]))
-print('Dimensions train_y: %s' % (train_y.shape))
-
-wi = np.random.choice([0,1], len(train_y))
 
 def sigma(x):
-    return (1+np.exp(-x))**-1
+    return 1./(1.+np.exp(-x))
 
 
-def logistic_regression(x):
-    pass
+def error(w, x, t, lam=0):
+    n = len(x)
+
+    y = sigma(np.dot(w, x.T))
+    E = -(np.dot(t, np.log(y)) + np.dot((1-t), np.log(1-y)))/n + lam/2 * (w**2).sum()
+
+    return E
 
 
-def gradient_descent(xi,wi):
-    pass
+def error_gradient(w, x, t, lam=0):
+    n, d = x.shape
+
+    y = sigma(np.dot(w, x.T))
+    E_grad = (np.dot((y-t), x) + lam * w)/n
+
+    return E_grad
 
 
-def plot():
-    pass
+def hessian(w, x, t, lam=0): # TODO Niet zeker nu wel d,d shape
+    n, d = x.shape
+    y = sigma(np.dot(w, x.T))[:,np.newaxis]
 
-# --------------------------------------
-# Momentum
+    h = (np.dot(np.dot(x.T, y), np.dot(x.T, (1-y)).T) + lam*np.identity(d))/n
 
-# --------------------------------------
-# Weight decay
+    return h
 
 
+def gradient_descent(w, train_x, train_t, test_x, test_t, learning_rate, iter):
+    train_error, test_error = [], []
+
+    for _ in tqdm(range(iter)):
+        w = w - learning_rate * error_gradient(w, train_x, train_t)
+        train_error.append(error(w, train_x, train_t))
+        test_error.append(error(w, test_x, test_t))
+
+    return w, train_error, test_error
+
+
+def gradient_descent_momentum(w, train_x, train_t, test_x, test_t, learning_rate, momentum, iter):
+    train_error, test_error = [], []
+    delta = 0
+
+    for _ in tqdm(range(iter)):
+        delta = -learning_rate * error_gradient(w, train_x, train_t) + momentum * delta
+        w = w + delta
+        train_error.append(error(w, train_x, train_t))
+        test_error.append(error(w, test_x, test_t))
+
+    return w, train_error, test_error
+
+
+def weight_decay(w, train_x, train_t, test_x, test_t, learning_rate, lam, iter):
+    train_error, test_error = [], []
+
+    for _ in tqdm(range(iter)):
+        w = w - learning_rate * error_gradient(w, train_x, train_t, lam)
+        train_error.append(error(w, train_x, train_t))
+        test_error.append(error(w, test_x, test_t))
+
+    return w, train_error, test_error
+
+
+def newton_method(w, train_x, train_t, test_x, test_t, learning_rate, lam, iter):
+    train_error, test_error = [], []
+
+    for _ in tqdm(range(iter)):
+        w = w - learning_rate * (
+            np.linalg.inv(hessian(w, train_x, train_t)) * 
+            error_gradient(w, train_x, train_t, lam))
+
+        train_error.append(error(w, train_x, train_t))
+        test_error.append(error(w, test_x, test_t))
+
+    return w, train_error, test_error
+
+
+def plot_error(title, train_error, test_error):
+    # x = np.linspace(1, len(train_error))
+    plt.plot(train_error, label='train error')
+    plt.plot(test_error, label='test error')
+    plt.title(title)
+    plt.ylabel('Error')
+    plt.xlabel('Iteration')
+    plt.legend()
+    plt.show()
+
+
+def main():
+    np.random.seed(0)
+
+    train_x, train_t = load_data(
+        'data/train-images-idx3-ubyte', 'data/train-labels-idx1-ubyte'
+        )
+    
+    test_x, test_t = load_data(
+        'data/t10k-images-idx3-ubyte', 'data/t10k-labels-idx1-ubyte'
+        )
+
+    print('shape train_x: %s x %s' % (train_x.shape[0], train_x.shape[1]))
+    print('shape train_t: %s' % (train_t.shape))
+
+    # Initialize weights between [-1, 1]
+    w = np.random.choice(20000, train_x.shape[1])/10000 - 1
+
+    # Gradient descent ------------------------------------
+    learning_rate = 0.9
+    w, train_error, test_error = gradient_descent(
+        w, train_x, train_t, test_x, test_t, learning_rate, 10000)
+    print(f"FINAL ERROR: {error(w, train_x, train_t)} and {error(w, test_x, test_t)}")
+    plot_error("Gradient Descent", train_error, test_error)
+
+    # Logistic regression with Momentum -------------------
+    # learning_rate = 0.9
+    # momentum = 0.5
+    # w, train_error, test_error = gradient_descent_momentum(
+    #     w, train_x, train_t, test_x, test_t, learning_rate, momentum, 10000)
+    # print(f"FINAL ERROR: {error(w, train_x, train_t)} and {error(w, test_x, test_t)}")
+    # plot_error("Gradient Descent", train_error, test_error)
+
+    # Logistic regression with weight decay ---------------
+    # learning_rate = 0.9
+    # lam = 0.1
+    # w, train_error, test_error = weight_decay(
+    #     w, train_x, train_t, test_x, test_t, learning_rate, lam, 5340)
+    # print(f"FINAL ERROR: {error(w, train_x, train_t)} and {error(w, test_x, test_t)}")
+    # plot_error("Gradient Descent", train_error, test_error)
+
+    # Logistic regression with Newton method --------------
+    # learning_rate = 0.9
+    # lam = 0.1
+    # w, train_error, test_error = newton_method(
+    #     w, train_x, train_t, test_x, test_t, learning_rate, lam, 10)
+    # print(f"FINAL ERROR: {error(w, train_x, train_t)} and {error(w, test_x, test_t)}")
+    # plot_error("Gradient Descent", train_error, test_error)
+
+
+if __name__ == "__main__":
+    main()
