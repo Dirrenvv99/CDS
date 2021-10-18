8a627435d6de8e41339f0769bb3de69da79407bf
Gerwin de Kruijf <gerwindekruijf@gmail.com>
2021-10-18 15:17:48 +0200

    Week 5: deep learning

--- Week 5/ConvNet.py
+++ Week 5/ConvNet.py
@@ -11,6 +11,16 @@ import matplotlib.pyplot as plt
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # Building the model
+activations = ['relu', 'tanh']
+optimizers = ['adam', 'adagrad', 'sgd']
+
+model_ex_4 = models.Sequential([
+    layers.Flatten(),
+    layers.Dense(40, activation='relu'),
+    layers.Dense(20, activation='relu'),
+    layers.Dense(10, activation='softmax')
+    ])
+
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -39,21 +49,48 @@ plt.legend(loc='lower right')
 
 # 3.
 '''
-Difference with relu and tanh:
+Params = 122,570
+Difference with ReLU and tanh:
+ReLU: Test: loss=0.8949, accuracy=0.7075
+tanh: Test: loss=0.9759, accuracy=0.6762
+Plot for tanh can be found in ex_3_tanh.png
+The reason why using tanh performs worse is because [UITLEG]
 
 Optimizers:
-- Adam
-- Adagrad
-- SGD
+- Adam: loss=0.8949, accuracy=0.7075
+- Adagrad: loss=1.4249, accuracy=0.4919
+- SGD: loss=1.1328, accuracy=0.6089
+Plots can be found in:
+- ex_3_adagrad.png
+- ex_3_sgd.png
 '''
 
 # 4.
 '''
 Parameters both networks:
-
+- epochs = 10
+- learning rate = 1e-3
+- loss = sparse_categorical_crossentropy
+- optimizer = Adam
 
 Number of parameters:
+CNN: 122,570
+MLP: 123,950
 
+MLP results for 10 epochs with a learning rate of 1e-3:
+loss=1.7072, accuracy=0.3809
+plot: ex_4_MLP_10epochs.png
+CNN was:
+loss=0.8949, accuracy=0.7075
 
+MLP for 100 epochs with a learning rate of 1e-4:
+loss=1.4717, accuracy=0.4787
+A plot can be found in the plots folder: ex_4_MLP_100epochs.png
+Results can be found in ex_4_log.log in logs folder.
+The model was largely overfitting.
+
+The CNN clearly outperforms the MLP. This is because the MLP includes too many 
+parameters leading to redudancy and overfitting. CNN is better for image classification 
+due to less parameters, whereas the MLP is fully connected making the network unseemingly large.
 '''
 
--- Week 5/perceptron.py
+++ Week 5/perceptron.py
@@ -27,7 +27,7 @@ model_1 = models.Sequential([
 
 model_2 = models.Sequential([
     layers.Flatten(),
-    layers.Dense(200, activation='relu'),
+    layers.Dense(300, activation='relu'),
     layers.Dense(50, activation='relu'),
     layers.Dense(num_classes, activation='softmax')
     ])
@@ -39,18 +39,6 @@ model_1_large = models.Sequential([
     ])
 
 
-model_ex_4 = models.Sequential([
-    layers.Flatten(),
-    layers.Dense(400, activation='relu'),
-    layers.Dense(200, activation='relu'),
-    layers.Dense(50, activation='relu'),
-    layers.Dense(20, activation='relu'),
-    layers.Dense(num_classes, activation='softmax')
-    ])
-
-model_ex_4.summary()
-
-
 def run(model, epochs=20, lr=1e-5):
     model.compile(optimizer=tf.keras.optimizer.Adam(learning_rate=lr),
                 loss='sparse_categorical_crossentropy',
@@ -58,7 +46,6 @@ def run(model, epochs=20, lr=1e-5):
 
     history = model.fit(train_images, train_labels, epochs=epochs,
                         validation_data=(test_images, test_labels))
-
 
     test_loss, test_acc = model.evaluate(test_images, test_labels)
 
@@ -75,33 +62,75 @@ def plot(history, test_loss, test_acc):
     plt.ylim([0.5, 1])
     plt.legend(loc='lower right')
 
+
+if __name__ == "__main__":
+    run(model=model_0)
+    run(model=model_1)
+    run(model=model_2)
+
+
 # ----------------------------
 # 1.
 '''
-Parameters:
+Parameters (standard):
 - Optimizer = Adam
-- Learning rate = 1e-5
+- Learning rate = 1e-3
 - Loss = Sparse Categorical CrossEntropy, Tensorflow states: Use this crossentropy loss function when there are two or more label classes.
 - Metrics = Accuracy
-- Epochs = 20
-
-Q: Is early stopping convenient?
-A: Yes it is
+- Epochs = 200
 
 Discussion:
+For 0 hidden layers (model_0), we've tried epochs 20,50,100,200 and learning rates 1e-{1,2,3,4,5} but it did not reach maximum test accuracy
+For epochs=200 and lr=1e-3, the test accuracy was: 0.2969000041484833
+The plot of the learning curve can be found in ex_1_hiddenlayers_0.png
+Total params = 30730
 
+For 1 hidden layer (model_1), the ideal learning rate was 1e-4
+The test accuracy was: 0.5141000151634216
+The model largely overfits, since the training accuracy increases while the validation accuracy slightly decreases
+Early stopping would therefore be convenient.
+The plot of the learning curve can be found in ex_1_hiddenlayers_1.png
+Total params = 616,610
+
+We've also tried a learning rate=1e-3 with epochs=400 but the training accuracy did not go above 0.6 
+while the validation accuracy could not get above 0.5.
+
+For 2 hidden layers (model_2), the learning rate=1e-4 and epochs=200.
+The plot can be found in ex_1_hiddenlayers_2.png
+Total params = 937460
+Early stopping would be very convenient since the validation accuracy decreases rapidly
+Test accuracy = 0.49619999527931213
+Best validation accuracy = 0.5333
 
 '''
 # ----------------------------
 # 2. 
 '''
 Parameters:
+- Linear layer of 10000 hidden units
 - Optimizer = Adam
-- Learning rate = 1e-5
+- Learning rate = 1e-3
 - Loss = Sparse Categorical CrossEntropy
 - Metrics = Accuracy
 - Epochs = 1000
+- Model = model_1_large
 
 Discussion, comparison with [https://arxiv.org/pdf/1611.03530.pdf]:
+Test accuracy: 50.51
+Train accuracy: 100
+Learning rate: 1e-2
 
+Our results for 1000 epochs with layer of 10000 units.
+Learning rate = 1e-4
+
+Params = 30830010
+After two hours of running: 
+- elapsed epochs = 68
+- highest training accuracy: 0.9140
+- highest validation accuracy: 0.5630
+The validation accuracy is higher in comparison with the baseline
+However, the model is largely overfitting on the data given that the 
+train accuracy improves while the validation accuracy reaches its maximum ≈ 0.56
+
+All results can be found in ex_2_log.log in folder logs
 '''
