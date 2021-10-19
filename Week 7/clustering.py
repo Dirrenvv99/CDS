61e2eea6b227a55bff57914e7fd18a3683ac4c8c
Gerwin de Kruijf <gerwindekruijf@gmail.com>
2021-10-19 20:46:23 +0200

    EM algorithm Week 7
    Old faithful works, mnist data not so much

--- Week 7/clustering.py
+++ Week 7/clustering.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 from mlxtend.data import loadlocal_mnist
+from numpy.core.shape_base import stack
+from numpy.lib.shape_base import dstack
 from tqdm import tqdm
 
 
@@ -15,30 +17,91 @@ def load_data(images_path, labels_path):
         labels_path=f'{week_4}/{labels_path}'
     )
 
-    # Take the 3's and 7's
-    x_3 = np.array([val for idx, val in enumerate(x) if y[idx] == 3])
-    x_7 = np.array([val for idx, val in enumerate(x) if y[idx] == 7])
+    x = x / 255
 
-    # 0: x = 3, 1: x = 7
-    x = np.concatenate((x_3, x_7))
-    t = np.concatenate((np.zeros(len(x_3)), np.ones(len(x_7))))
-
-    x = x / 255                    # normalise x
-    x = np.insert(x, 0, 1, axis=1) # append 1 for w_0
-
-    return x, t
+    return x, y
 
 
-train_x, train_t = load_data(
+class GMM:
+    # TODO: https://towardsdatascience.com/gaussian-mixture-models-implemented-from-scratch-1857e40ea566
+    # Extra: https://www.analyticsvidhya.com/blog/2019/10/gaussian-mixture-models-clustering/
+    def __init__(self, clusters):
+        self.K = clusters
+
+    def gaussian(self, x, mu, sigma, k):
+        # Values are split up to get more accurate error detection
+        d = (x - mu).T
+        s1 = np.linalg.inv(sigma)
+        s = np.matmul(d, s1)
+        e = np.matmul(s, d)
+        n = np.exp(-1/2*e)
+        d = np.sqrt((2*np.pi)**k * np.linalg.det(sigma))
+        return n/d
+
+    def log_likelihood(self, X, mu, sigma, pi):
+        logs = [np.log(np.sum([pi[k] * self.gaussian(x, mu[k], sigma[k], k) for k in range(self.K)])) for x in X]
+        return np.sum(logs)
+
+    # EM-Algorithm
+    def fit(self, X, max_iter=100, min_diff=0.01):
+        # 1. Initialization
+        # https://link.springer.com/article/10.3758/s13428-015-0697-6
+        print(f'Data: {X.shape}')
+        N, d = X.shape
+        random_row = np.random.randint(low=0, high=N, size=self.K)
+        mu = np.array([X[row_index,:] for row_index in random_row])
+        sigma = np.array([np.cov(X.T) for _ in range(self.K)])
+        pi = np.full(self.K, 1/self.K)
+
+        print(f'Means: {mu.shape}, Covariances: {sigma.shape}, Coefficients: {pi.shape}')
+
+        llh = [self.log_likelihood(X, mu, sigma, pi)]
+        print(f'[{len(llh) -1}]: log likelihood = {llh[-1]}')
+
+        gamma = np.zeros((N, self.K))
+
+        for iter in range(max_iter):
+            # 2. E-step
+            print(f'E step {iter+1}')
+            for k in range(self.K):
+                for n in range(len(X)):
+                    d = np.sum([pi[j] * self.gaussian(X[n], mu[j], sigma[j], j) for j in range(self.K)])
+                    gamma[n, k] = pi[k] * self.gaussian(X[n], mu[k], sigma[k], self.K) / d
+
+            # 3. M-step
+            print(f'M step {iter+1}')
+            for k in range(self.K):
+                N_k = sum(gamma[:,k])
+                mu[k] = 1 / N_k * np.sum([gamma[n,k] * X[n] for n in range(N)], axis=0)
+                sigma[k] = 1 / N_k * np.sum([gamma[n,k] * (np.matmul((X[n] - mu[k])[np.newaxis].T, (X[n]-mu[k])[np.newaxis])) for n in range(N)], axis=0)
+                pi[k] = N_k / N
+
+            # 4. Evaluate log likelihood
+            llh.append(self.log_likelihood(X, mu, sigma, pi))
+            diff = np.abs(llh[-2] - llh[-1])
+            print(f'[{len(llh) -1}]: log likelihood = {llh[-1]}')
+            print(f'[{len(llh) -1}]: difference = {diff}')
+            # This is to stop the algorithm from continueing for too long
+            if diff < min_diff:
+                print(f'[{len(llh) -1}]: Changes are too small to continue')
+                break
+
+        return mu, sigma, gamma, llh
+
+
+def main():
+    train_x, train_t = load_data(
     'data/train-images-idx3-ubyte', 'data/train-labels-idx1-ubyte'
     )
 
-test_x, test_t = load_data(
+    test_x, test_t = load_data(
     'data/t10k-images-idx3-ubyte', 'data/t10k-labels-idx1-ubyte'
     )
 
+    gmm = GMM(10)
+    X = np.array(train_x)
+    gmm.fit(X)
 
-def clustering():
-    # TODO: https://towardsdatascience.com/gaussian-mixture-models-implemented-from-scratch-1857e40ea566
-    # Extra: https://www.analyticsvidhya.com/blog/2019/10/gaussian-mixture-models-clustering/
-    pass
+
+if __name__=="__main__":
+    main()
--- Week 7/em.py
+++ Week 7/em.py
@@ -1,4 +1,6 @@
 import numpy as np
+from clustering import GMM
+import matplotlib.pyplot as plt
 
 def load_data():
     """
@@ -15,10 +17,31 @@ def load_data():
     return np.array([[e[1], e[2]] for e in data])
 
 
+def plot(X, gamma, K, colors):
+    # get the assigned labels
+    labels = np.argmax(gamma, axis = 1)
+
+    fig, ax = plt.subplots()
+    for k in range(K):
+        cluster = np.where(labels == k)
+        x = X[cluster]
+        ax.scatter(x[:,0], x[:,1], c = colors[k], label = k, alpha = 0.7)
+
+    plt.title('Old Faithful Data')
+    plt.xlabel('eruptions')
+    plt.ylabel('waiting')
+    ax.legend()
+    plt.show()
+
+    # TODO: plot ellipse
+    # TODO: recreate figure 11.11 : 6 plots
+
 
 def main():
-    # np.random.seed(0)
-    print(load_data())
+    data = load_data()
+    gmm = GMM(2)
+    mu, sigma, gamma, llh = gmm.fit(data, 30, 0.001)
+    plot(data, gamma, 2, ['darkred', 'darkblue'])
 
 
 if __name__ == "__main__":
