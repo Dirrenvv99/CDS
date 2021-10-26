1fe19a4cc19b045106a4b5ec21ab91d100d88389
Gerwin de Kruijf <gerwindekruijf@gmail.com>
2021-10-26 23:21:12 +0200

    BIEM!

--- Week 7/gmm.py
+++ Week 7/gmm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
+from matplotlib.patches import Ellipse
 
 
 class GMM:
